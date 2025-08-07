e52df3dc4cdbaa73c8fc0f11b43d5a595eb8dfb1
Mitul Italiya <mitulitaliya2555@gmail.com>
2025-08-07 13:21:58 +0530

    Fix Header UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,67 +1,63 @@
 import React from "react";
-import "./Header.css";
-import logo from "../../assets/Logo/logo.png";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-   
     <>
-    
-    <title>Refined Grains</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <nav className="navbar navbar-expand-lg bg-white  py-2">
+    <nav className="navbar navbar-expand-lg">
       <div className="container">
-     
-        <a className="navbar-brand d-flex align-items-center" href="#">
-          <img src={logo} alt="Refined Grains" className="logo" />
-        </a>
-     
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Menu items */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
-              <a className="nav-link active" href="#" style={{color:"#A37C4D"}}>
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Shop
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Customise Wheat
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-          {/* Login Button */}
-         <button class="login-btn">LOGIN</button>
+        <div className={'nav-background'}>
+          <Link  className="navbar-brand" to={"/home"}>
+            <img src={logo} alt="Refined Grains" className="logo img-fluid"/>
+          </Link>
+
+          <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"/>
+          </button>
+
+          {/* Menu items */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" to={'/home'}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Shop
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Customise Wheat
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            {/* Login Button */}
+            <button className="login-btn">Login</button>
+          </div>
         </div>
+
       </div>
     </nav>
     </>
