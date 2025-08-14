e0d7575b495bd6c95d09ac13b82aa7751ac73934
Mitul Italiya <mitulitaliya2555@gmail.com>
2025-08-14 13:35:59 +0530

    Login modal Add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/images/logo.png";
+import RightArrow from "../../assets/svg/arrow-right-white.svg";
 import { Link, NavLink } from "react-router-dom";
 import profile from "../../assets/images/profile.jpg";
 
@@ -21,6 +22,8 @@ const Header = () => {
   ];
 
   return (
+
+      <>
       <nav className="navbar navbar-expand-lg p-0 position-fixed w-100 z-3">
         <div className="container">
           <div className={"nav-background"}>
@@ -56,25 +59,69 @@ const Header = () => {
 
             {/* Login/Profile Section */}
             <div className={"d-flex align-items-center"}>
-              {!isLoggedIn ? (
-                  <button
-                      className="login-btn"
-                      onClick={() => {
-                        localStorage.setItem("authToken", "dummy_token");
-                        setIsLoggedIn(true);
-                      }}
-                  >
-                    Login
-                  </button>
-              ) : (
-                  <Link to={"/my-order"} className={"profile-img ml_14"}>
-                    <img src={profile} alt="user_profile" className={"user-img"} />
-                  </Link>
-              )}
+              <button className="login-btn" data-bs-toggle="modal" data-bs-target="#loginModal" > Login </button>
+              <Link to={"/my-order"} className={"profile-img ml_14"}>
+                <img src={profile} alt="user_profile" className={"user-img"} />
+              </Link>
+              {/*{!isLoggedIn ? (*/}
+              {/*    <button*/}
+              {/*        className="login-btn"*/}
+              {/*        onClick={() => {*/}
+              {/*          localStorage.setItem("authToken", "dummy_token");*/}
+              {/*          setIsLoggedIn(true);*/}
+              {/*        }}*/}
+              {/*    >*/}
+              {/*      Login*/}
+              {/*    </button>*/}
+              {/*) : (*/}
+              {/*    <Link to={"/my-order"} className={"profile-img ml_14"}>*/}
+              {/*      <img src={profile} alt="user_profile" className={"user-img"} />*/}
+              {/*    </Link>*/}
+              {/*)}*/}
             </div>
           </div>
         </div>
       </nav>
+        {/* Bootstrap Login Modal */}
+        <div className="modal fade" id="loginModal" tabIndex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+
+              <div className="login">
+                <div className="login-header mb_80 d-flex align-items-center justify-content-between gap-2">
+                  <div className="d-flex align-items-center">
+                    <h5 className={'fs_20 fw_400 text-black m-0 mr_6'}>Welcome to</h5>
+                    <img src={logo} alt="Refined Grains" className="logo" />
+                  </div>
+                  <p className={'fs_13 fw_400 text-color'}>
+                    No Account ?
+                    <a href="#">Log in</a>
+                  </p>
+                </div>
+
+                <div className="login-body">
+                  <h2 className={'fs_55 fw_500 text-black lh-1 mb_98'}> Log in </h2>
+                  <label className="label d-block fs_24 fw_600 mb_38">
+                    Enter your mobile number
+                  </label>
+
+                  <div className="mobile-input-wrapper d-flex align-items-center">
+                    <div className="country-code-box mr_32">
+                      <span className="fw-bold fs_20 fw_600 pb_10 d-block">+91</span>
+                    </div>
+                    <input type="text" placeholder="Mobile number" className="mobile-number-input"/>
+                  </div>
+
+                  <button className={'button btn-solid text-uppercase mt_68 d-block mx-auto'}>
+                    CONTINUE<img src={RightArrow} alt="Refined Grains" className="img-fluid ml_10" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </>
   );
 };
 
