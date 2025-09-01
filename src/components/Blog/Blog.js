f8fdf1e15de59e47d89d654cb926d216d6291e08
deviradadiya <deviradadiya7@gmail.com>
2025-09-01 16:26:22 +0530

    blog page are responssive and other sone changes

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -18,35 +18,34 @@ const Blog = () => {
   return (
     <div>
       <section className="news-section">
-        <h1 className="mb_30 fs_40 fw_700">Discover Our latest News</h1>
+        <h1 className="mb_30 fw_700">Discover Our latest News</h1>
         <div className="news-container">
           {/* Left Main News */}
           <div className="main-news ">
             <Link to="/blogdetails">
               <img src={disnews} alt="AI" />
             </Link>
-            <div className="main-news-content pt_40  fw_600">
-              <h3 className="fs_20 fw_600">Artificial Intelligence</h3>
-              <h2 className="main-news-para fs_24 fw_700 pt_30">
+            <div className="main-news-content pt_26  fw_600">
+              <h3 className=" fw_600">Artificial Intelligence</h3>
+              <h2 className="main-news-para fw_700 pt_30">
                 Harnessing Artificial Intelligence to Revolutionize Workflow
                 Automation and Drive Smarter Business Decisions
               </h2>
-              <p className="pt_30 fs_20">Olivia Thompson • Aug 10, 2025</p>
+              <p className="pt_30  pb_30">Olivia Thompson • Aug 10, 2025</p>
             </div>
           </div>
           {/* Right Side News */}
           <div className="side-news">
             <div className="news-card">
               <img src={cloud} alt="Cloud" />
-              <div className="news-card-content pl_24">
-                <h5 className="fw_600 fs_16 pt_8 ">Cloud Computing</h5>
-                <h4 className="fs_16 fw_700 pt_30">
-                  Cloud Transformation Roadmap: <br /> How Businesses Can
-                  Achieve
+              <div className="news-card-content">
+                <h5 className="pt_14">Cloud Computing</h5>
+                <h4 className=" pt_40">
+                  Cloud Transformation Roadmap: How Businesses Can Achieve
                   <br />
                   Scalability, Security, and Cost E...
                 </h4>
-                <p className="fs_14 fw_400 pt_30">
+                <p className="pt_50">
                   James Carter <span className="fw-bold"> • </span>Aug 12, 2025
                 </p>
               </div>
@@ -54,28 +53,28 @@ const Blog = () => {
 
             <div className="news-card">
               <img src={cyber} alt="Cybersecurity" />
-              <div className="news-card-content pl_24">
-                <h5 className="fw_600 fs_16 pt_8 ">Cybersecurity</h5>
-                <h4 className="fs_16 fw_700 pt_30">
+              <div className="news-card-content">
+                <h5 className=" pt_14 ">Cybersecurity</h5>
+                <h4 className=" pt_24">
                   Building a Resilient Cybersecurity Framework to Protect Your
                   <br />
                   Organization from Advanced Thr...
                 </h4>
-                <p className="fs_14 fw_400 pt_30">
+                <p className=" pt_50">
                   Daniel Smith <span className="fw-bold"> • </span> Aug 08, 2025
                 </p>
               </div>
             </div>
             <div className="news-card">
               <img src={development} alt="Web Development" />
-              <div className="news-card-content pl_24">
-                <h5 className="fw_600 fs_16 pt_8">Web Development</h5>
-                <h4 className="fs_16 fw_700 pt_30">
+              <div className="news-card-content">
+                <h5 className="pt_14 ">Web Development</h5>
+                <h4 className="pt_24">
                   From Monolithic to Microservices: Modern Web Development{" "}
                   <br />
                   Strategies for High-Performance...
                 </h4>
-                <p className="fs_14 fw_400 pt_30">
+                <p className="pt_50">
                   Emily Johnson <span className="fw-bold"> • </span> Aug 06,
                   2025
                 </p>
@@ -87,8 +86,8 @@ const Blog = () => {
 
       <div className="container pt_100">
         {/* Heading */}
-        <div>
-          <h2 className="page-title   text-center fw_700   fs_40">
+        <div className="card-wrapper">
+          <h2 className="page-title   text-center ">
             Insights, Innovations, and IT Trends That <br /> Power the Future
           </h2>
           {/* Grid Section */}
@@ -97,10 +96,10 @@ const Blog = () => {
             <div className="card ">
               <img src={data} alt="Big Data" />
               <div className="card-details">
-                <p className="category pt_24  fw_600 fs_16 pb_30">
+                <p className="category pt_24 fw_600 pr-7  pb_30 ">
                   Data Analytics
                 </p>
-                <h3 className="fw_700  fs_24 pb_30">
+                <h3 className="card_desc pb_30">
                   Unlocking the Power of Big Data: Turning Complex Datasets into
                   Actionable Insights for Competiti...
                 </h3>
@@ -110,86 +109,98 @@ const Blog = () => {
             {/* Card 2 */}
             <div className="card">
               <img src={stratgy} alt="IT Strategy" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                IT Strategy
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Future–Proofing Your Business IT Infrastructure to Thrive in an
-                Ever–Changing Digital Landscape
-              </h3>
-              <p className="author">Sarah Williams • Aug 02, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600 pb_30">IT Strategy</p>
+                <h3 className="card_desc pb_30">
+                  Future–Proofing Your Business IT Infrastructure to Thrive in
+                  an Ever–Changing Digital Landscape
+                </h3>
+                <p className="author">Sarah Williams • Aug 02, 2025</p>
+              </div>
             </div>
             {/* Card 3 */}
             <div className="card">
               <img src={cybersecurity} alt="Cybersecurity" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Cybersecurity
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Strengthening Your Cybersecurity Posture to Defend Against
-                Next-Generation Threats
-              </h3>
-              <p className="author">David Anderson • Aug 05, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7  fw_600 pb_30">
+                  Cybersecurity
+                </p>
+                <h3 className="card_desc pb_30">
+                  Strengthening Your Cybersecurity Posture to Defend Against
+                  Next-Generation Threats
+                </h3>
+                <p className="author">David Anderson • Aug 05, 2025</p>
+              </div>
             </div>
             {/* Card 4 */}
             <div className="card pb_50">
               <img src={cloud_card} alt="Cloud Computing" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Cloud Computing
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Maximizing Scalability and Security Through Hybrid Cloud
-                Architecture in 2025
-              </h3>
-              <p className="author">Mark Johnson • Aug 09, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600 pb_30">
+                  Cloud Computing
+                </p>
+                <h3 className="card_desc pb_30">
+                  Maximizing Scalability and Security Through Hybrid Cloud
+                  Architecture in 2025
+                </h3>
+                <p className="author">Mark Johnson • Aug 09, 2025</p>
+              </div>
             </div>
             {/* Card 5 */}
             <div className="card">
               <img src={cybers} alt="Zero Trust Security" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Cybersecurity
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Implementing Zero Trust Security to Protect Your Business from
-                Evolving Cyber Threats
-              </h3>
-              <p className="author">Laura Mitchell • Aug 07, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600 pb_30">
+                  Cybersecurity
+                </p>
+                <h3 className="card_desc pb_30">
+                  Implementing Zero Trust Security to Protect Your Business from
+                  Evolving Cyber Threats
+                </h3>
+                <p className="author">Laura Mitchell • Aug 07, 2025</p>
+              </div>
             </div>
             {/* Card 6 */}
             <div className="card">
               <img src={artifical} alt="AI" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Artificial Intelligence
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Using AI–Driven Analytics to Gain Competitive Insights and
-                Improve Decision–Making
-              </h3>
-              <p className="author">William Parker • Aug 05, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600  pb_30">
+                  Artificial Intelligence
+                </p>
+                <h3 className="card_desc">
+                  Using AI–Driven Analytics to Gain Competitive Insights and
+                  Improve Decision–Making
+                </h3>
+                <p className="author">William Parker • Aug 05, 2025</p>
+              </div>
             </div>
             {/* Card 7 */}
             <div className="card">
               <img src={dataanalitics} alt="Data First" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Data Analytics
-              </p>
-              <h3 className="fw_700 pb_30 fs_24 ">
-                Building a Data–First Culture to Unlock Insights and Drive
-                Business Growth
-              </h3>
-              <p className="author ">Emma Richardson • Aug 03, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600 pb_30">
+                  Data Analytics
+                </p>
+                <h3 className="card_desc pb_30  ">
+                  Building a Data–First Culture to Unlock Insights and Drive
+                  Business Growth
+                </h3>
+                <p className="author ">Emma Richardson • Aug 03, 2025</p>
+              </div>
             </div>
             {/* Card 8 */}
             <div className="card">
               <img src={webdevelopment} alt="Headless CMS" />
-              <p className="category  pt_24 pr-7 fw_600 fs_16 pb_30">
-                Web Development
-              </p>
-              <h3 className="fw_700  fs_24 pb_30">
-                Adopting Headless CMS for Faster, More Flexible Digital
-                Experiences
-              </h3>
-              <p className="author">Daniel Cooper • Aug 01, 2025</p>
+              <div className="card-details">
+                <p className="category  pt_24 pr-7 fw_600 pb_30">
+                  Web Development
+                </p>
+                <h3 className="card_desc pb_30">
+                  Adopting Headless CMS for Faster, More Flexible Digital
+                  Experiences
+                </h3>
+                <p className="author">Daniel Cooper • Aug 01, 2025</p>
+              </div>
             </div>
           </div>
         </div>
